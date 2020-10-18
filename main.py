0097a2cfe13bac5399e8dd7ecfa5ad26942eacec
waffles <0@waffles.tech>
2020-10-17 23:16:31 -0500

    Fixed dynamic config on bot logon

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import Config
 
 bot = commands.Bot(command_prefix=Config.PREFIX)
 
-cogs = ['cogs.admin','cogs.ama']
+cogs = ['cogs.admin', 'cogs.ama']
 
 cluster = Config.CLUSTER
 db = cluster["amabot"]
 questions = db["questions"]
+
 
 @bot.event
 async def on_ready():
@@ -16,21 +17,24 @@ async def on_ready():
     print("______________")
     print("Loading extensions...")
     try:
-        for cog in cogs: 
-    	    bot.load_extension(cog)
+        for cog in cogs:
+            bot.load_extension(cog)
     except commands.ExtensionError as e:
         print(f"Error loading extensions: {e.__class__.__name__}: {e}")
     else:
         print("All extensions loaded successfully.")
     print("______________")
-    dynamicconfig = questions.find_one(({"_id":"1","paused":"No"}))
+    dynamicconfig = questions.find_one(({"_id": "1"}))
     if dynamicconfig:
         print("Found dynamic config document.")
+        print("Resetting dynamic config document...")
+        questions.update_one({"_id": "1"}, {"$set": {"paused": "No"}})
+        print("Found and reset dynamic config document.")
         print("______________")
     if not dynamicconfig:
         print("No dynamic config document found, creating one...")
-        questions.insert_one(({"_id":"1","paused":"No"}))
-        print("Created dynamic config document.")    
+        questions.insert_one(({"_id": "1", "paused": "No"}))
+        print("Created dynamic config document.")
         print("______________")
 
 bot.run(Config.TOKEN)
