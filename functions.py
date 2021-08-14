a9e3aaaf502f0b895d75b4cf18d22eb2ec8675ec
Benson <benson@kitia.dev>
2021-08-14 03:31:18 +0000

    Error messages no longer use embeds 🏃‍♂️

--- functions.py
+++ functions.py
@@ -5,21 +5,7 @@ import os
 class Functions:
 
     async def handle_error(self, ctx, description : str, troubleshooting : str = None):
-        embed = discord.Embed(
-            description = description,
-            color = 0xf34141
-            )
-        embed.set_author(name = "Error")
-        if troubleshooting:
-            embed.set_footer(text = troubleshooting)
-        await ctx.send(embed = embed)
+        await ctx.send(f":warning: Something went wrong: {description}. {troubleshooting}")
     
     async def confirm_action(self, ctx, description : str, additional_info: str = None):
-        embed = discord.Embed(
-            description = description,
-            color = 0x43e286
-        )
-        embed.set_author(name = "Success")
-        if additional_info:
-            embed.set_footer(text = additional_info)
-        await ctx.send(embed = embed)
+        await ctx.send(f":white_check_mark: {description}. {additional_info}")
